919a54b160ac1a68158e7f02fbccc4af39c88028
Adithya B V <adithyab22@karunya.edu.in>
2025-05-30 12:36:04 +0530

    Files Added

---
+++ backend/products.js
@@ -0,0 +1,31 @@
+const products = [
+  {
+    id: 1,
+    name: "iPhone 12 Pro",
+    brand: "Apple",
+    desc: "6.1-inch display",
+    price: 999,
+    image:
+      "https://res.cloudinary.com/dnynhn7k4/image/upload/v1743917927/iphone12pro_kf5jd1.jpg",
+  },
+  {
+    id: 2,
+    name: "iPhone 12",
+    brand: "Apple",
+    desc: "5.4-inch mini display",
+    price: 699,
+    image:
+      "https://res.cloudinary.com/dnynhn7k4/image/upload/v1743917949/iphone12_ymo0ru.jpg",
+  },
+  {
+    id: 3,
+    name: "Galaxy S",
+    brand: "Samsung",
+    desc: "6.5-inch display",
+    price: 399,
+    image:
+      "https://res.cloudinary.com/dnynhn7k4/image/upload/v1743917961/galaxyS_n70ydx.png",
+  },
+];
+
+module.exports = products;
